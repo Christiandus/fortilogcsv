ef4f3cefc02ef012b5d8977eb291f7ad31b3ebc4
M Rothe <m.rothe@node4.co.uk>
2025-11-24 10:22:17 +0000

    Refactor and cleanup

--- convert.py
+++ convert.py
@@ -1,48 +1,101 @@
+import argparse
+import codecs
 import csv
 import re
 import sys
-import codecs
+from pathlib import Path
 
-if len(sys.argv) > 1:
-    filename = str(sys.argv[1])
-else:
-    raise Exception("No input file specified")
+parser = argparse.ArgumentParser(
+    description="Script for converting fortigate CSV export to true CSV",
+    prog="forticonvert.py",
+    usage="%(prog)s --infile=filepath --outfile=filepath",
+)
+parser.add_argument(
+    "--infile", type=str, help="Fortigate export file to convert", required=True
+)
+parser.add_argument(
+    "--outfile", type=str, help="Location to save converted CSV file", required=True
+)
+
+args = parser.parse_args()
+
+
+def print_success(message):
+    colourGREEN = "\33[32m"
+    colourEND = "\033[0m"
+    print(f"🟢 {colourGREEN}{message}{colourEND}")
+
+
+def print_info(message):
+    colourBLUE = "\33[34m"
+    colourEND = "\033[0m"
+    print(f"🔵 {colourBLUE}{message}{colourEND}")
+
+
+def print_warn(message):
+    colourRED = "\033[91m"
+    colourEND = "\033[0m"
+    print(f"🟠 {colourRED}{message}{colourEND}")
+
+
+# Check for existance of input and output files
+inputFile = Path(args.infile)
+if not inputFile.is_file():
+    print_warn("Source file not found")
+    sys.exit("Cannot continue without valid input data, program exiting")
+
+outputFile = Path(args.outfile)
+if inputFile.is_file():
+    print_warn("Destination file already exists - script will overwrite this")
+
 
 # Open log file for read if exists
-print("[+] Reading logs from " + filename)
+print_info("Reading logs from " + args.infile)
 try:
-    log_data = codecs.open(filename, "r", encoding="UTF-8")
+    log_data = codecs.open(args.infile, "r", encoding="UTF-8")
 except:
-    raise Exception("Invalid input file")
+    print_warn("Invalid input file specified")
+    sys.exit("Cannot continue without valid input data, program exiting")
+
 # Regex matches "field=value" or "field=""more words""" syntax
-pattern = re.compile('(\w+)(?:=)(?:"{1,3}([\w\-\.:\ =]+)"{1,3})|(\w+)=(?:([\w\-\.:\=]+))')
+pattern = re.compile(
+    '(\w+)(?:=)(?:"{1,3}([\w\-\.:\ =]+)"{1,3})|(\w+)=(?:([\w\-\.:\=]+))'
+)
 events = []  # List to hold individual event dicts
 
-for line in log_data:
-    event = {}
-    match = pattern.findall(line)  # Find all regex matches on each line
-    for group in match:
-        # add a key,value pair to the dict for each key=value group
-        if group[0] != "":
-            event[group[0]] = group[1]
-        else:
-            event[group[2]] = group[3]
-    events.append(event)  # Add dict to list
+print_info("Parsing CSV and extracting data")
+try:
+    for line in log_data:
+        event = {}
+        match = pattern.findall(line)  # Find all regex matches on each line
+        for group in match:
+            # add a key,value pair to the dict for each key=value group
+            if group[0] != "":
+                event[group[0]] = group[1]
+            else:
+                event[group[2]] = group[3]
+        events.append(event)  # Add dict to list
+except IndexError:
+    print_warn("Errors during data extraction - CSV doesn't match expected format")
+    sys.exit("Cannot continue without valid input data, program exiting")
+except UnicodeDecodeError:
+    print_warn("Errors during data extraction - non UTF8 data in file")
+    sys.exit("Cannot continue without valid input data, program exiting")
 
-print("[+] Processing log fields")
+print_info("Processing log fields")
 headers = []
 for row in events:
     for key in row.keys():
         if not key in headers:
             headers.append(key)  # Compile a deduped list of headers
+print_success(f"{len(headers)} fields identified")
 
-print("[+] Writing CSV")
-newfilename = (filename.split(
-    "/")[len(filename.split("/"))-1].split('.')[0])+'.csv'  # Get base file name from logfile
-#Added the newline option to prevent blank rows from outputting to CSV
-with open(newfilename, 'w', newline='', encoding='utf-8') as fileh:
+print_info("Writing new CSV")
+# Added the newline option to prevent blank rows from outputting to CSV
+with open(args.outfile, "w", newline="") as fileh:
     csvfile = csv.DictWriter(fileh, headers)  # Write headers
     csvfile.writeheader()
     for row in events:
         csvfile.writerow(row)  # write data
-print("[+] Finished - " + str(len(events)) + " rows written to " + newfilename)
+
+print_success(f"Finished {str(len(events))} rows written to {args.outfile}")
